46973648bcd47a3beba5f27366033743ca5c5b5b
Hugo Valdés <hugo.valdes.dev@gmail.com>
2023-12-20 20:22:54 -0300

    Commit inicial

---
+++ SistemaVentas.IOC/Dependencia.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SistemaVentas.DAO.DBContext;
+
+namespace SistemaVentas.IOC
+{
+    public static class Dependencia
+    {
+        public static void DependencyInyect(this IServiceCollection services, IConfiguration configuration)
+        {
+             services.AddDbContext<DbventaContext>(options =>
+             {
+                 options.UseSqlServer(configuration.GetConnectionString("CadenaSQL"));
+             });
+        }
+    }
+}
